ee9479b5eccfef7a924f6338d133282d084c147c
AmiyaSharma <yash7773@gmail.com>
2023-10-19 17:42:36 +0530

    complete

--- office/todoapp/script.js
+++ office/todoapp/script.js
@@ -1,133 +1,41 @@
-let button= document.querySelector("form")
 
-button.addEventListener("submit", submitform)
+function newTask() {
+  var taskText = document.getElementById("taskInput").value;
+  if (taskText === "") {
+    alert ("Please enter the task")
+  }
 
-
-function submitform(){
-
-    
- 
-  let name = document.getElementById("text").value
-
-let obj={
-  name:name,};
-console.log(obj)
-
-let tasklist =JSON.parse(localStorage.getItem("task-list"))||[]
-tasklist.push(obj)
-localStorage.setItem("task-list",JSON.stringify(tasklist))
-
-}
-
-let parent = document.getElementById("data1")
-
-let data =JSON.parse(localStorage.getItem("task-list"))||[]
-appenddata(data);
-function appenddata(data){
-parent.innerHTML=""
-  data.forEach(function(ele,ind){
-    let tr= document.createElement("tr")
-    let td1=document.createElement("td")
-    td1.textContent=ele.name;
-    
-    
-
-
-
-
-
-
-
-    let td7=document.createElement("button")
-    td7.textContent="Remove";
-    td7.addEventListener("click", function(){
-let ele = data.splice(ind,1);
-let priortylist =JSON.parse(localStorage.getItem("priority-list"))||[]
-priortylist.push(ele[0])
-localStorage.setItem("priority-list",JSON.stringify(priortylist));
-localStorage.setItem("task-list",JSON.stringify(data));
-appenddata(data);
-
-    })
-    tr.append(td1, td7)
-    parent.append(tr);
-  })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let user_data = JSON.parse(localStorage.getItem("account-data"))
-
-
-// let formdata=document.querySelector("form")
-
-// formdata.addEventListener("submit", newdata)
-
-// let data= JSON.parse(localStorage.getItem("Helpers"))||[]
-
-// function newdata(){
-    
-//     let taskname = document.getElementById("text").value
-//     let user = {taskname}
-
-//     data.push(user)
-// localStorage.setItem("Helpers",JSON.stringify(data))
-    
-// }
-
-// let userdata= JSON.parse(localStorage.getItem("Helpers"))||[]
-
-// console.log(userdata)
-
-// let tbody= document.getElementById("data1")
-
-
-// function showdata(){
+  var li = document.createElement("li");
+  li.innerText = taskText;
   
-//     userdata.forEach((user) => {
-//         let tr=document.createElement("tr")
-//         let taskname= document.createElement("td")
-//         let remove=document.createElement("td")
-//         remove.addEventListener("click", function(){
-//             let ele = data.splice(ind,1);
-//             let priortylist =JSON.parse(localStorage.getItem("priority-list"))||[]
-//             priortylist.push(ele[0])
-//             localStorage.setItem("priority-list",JSON.stringify(priortylist));
-//             localStorage.setItem("Helpers",JSON.stringify(userdata));
-//             showdata(userdata);
-            
-//                 })
+  
+  var span = document.createElement("span");
+  span.className = "close";
+  span.innerText = "X";
+  span.onclick = function() {
+      var taskList = document.getElementById("taskList");
+      taskList.removeChild(li);
+  };
+
+  li.appendChild(span);
+  
+ 
+  li.addEventListener("click", function() {
+      li.classList.toggle("checked");
+  });
+
+  var taskList = document.getElementById("taskList");
+  taskList.appendChild(li);
+  
+  document.getElementById("taskInput").value = "";
+}
 
 
+document.getElementById("addTask").addEventListener("click", newTask);
 
-        
-// // taskname.textContent=user.taskname
-// // remove.textContent="Delete"
-// tr.append(taskname, remove)
 
-// tbody.append(tr)
-
-//     });
-// }
-
-// showdata()
+document.getElementById("taskInput").addEventListener("keyup", function(event) {
+  if (event.key === "Enter") {
+      newTask();
+  }
+});
