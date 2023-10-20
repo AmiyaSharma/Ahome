39c4a941974c1024e227d2dcf9db8b0f54b6aff8
AmiyaSharma <yash7773@gmail.com>
2023-10-20 18:49:36 +0530

    completebyapi

--- office/remdomqotegenrator/script.js
+++ office/remdomqotegenrator/script.js
@@ -1,33 +1,70 @@
-// Array of quotes with authors
-const quotes = [
-    {
-        quote: "The only way to do great work is to love what you do.",
-        author: "Steve Jobs"
-    },
-    {
-        quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
-        author: "Martin Luther King Jr."
-    },
-    {
-        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-        author: "Winston Churchill"
-    },
-    // Add more quotes here
-];
 
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const generateButton = document.getElementById("generate");
+// let quotes = [
+//     {
+//         quote: "The only way to do great work is to love what you do.",
+//         author: "Steve Jobs"
+//     },
+//     {
+//         quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+//         author: "Martin Luther King Jr."
+//     },
+//     {
+//         quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+//         author: "Winston Churchill"
+//     },
+   
+// ];
+
+// 
+
+const quotes = document.getElementById("quotes");
+const author = document.getElementById("author")
+const newq = document.getElementById("newq")
+let realdata="";
 
 
-function generateRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
-    quoteText.textContent = randomQuote.quote;
-    authorText.textContent = `- ${randomQuote.author}`;
+const getnewquote = ()=>{   
+    let random= Math.floor(Math.random()*10);
+//     console.log(realdata[random].text)  
+    
+// console.log(realdata[random].author)
+quotes.innerText = `${realdata[random].text}`
+author.innerText = `${realdata[random].author}`
 }
 
+const getquote = async()=>{
+    const api ="https://type.fit/api/quotes" 
+try {
+    let data = await fetch(api);
+     realdata =  await data.json()
+    // console.log(realdata.text)
+    getnewquote();
 
-generateButton.addEventListener("click", generateRandomQuote);
+} catch (error) {
+    console.log(error)
+}
 
-generateRandomQuote();
+}
+
+newq.addEventListener("click", getnewquote)
+    
+getquote()
+
+
+
+// const quoteText = document.getElementById("quote");
+// const authorText = document.getElementById("author");
+// const generateButton = document.getElementById("generate");
+
+
+// function generateRandomQuote() {
+//     const randomIndex = Math.floor(Math.random() * quotes.length);
+//     const randomQuote = quotes[randomIndex];
+//     quoteText.textContent = randomQuote.quote;
+//     authorText.textContent = `- ${randomQuote.author}`;
+// }
+
+
+// generateButton.addEventListener("click", generateRandomQuote);
+
+// generateRandomQuote();
